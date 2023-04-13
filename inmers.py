a056a6dc8e77e552f8588347f98b34897db70b87
Axel <axelfernandezcurros@gmail.com>
2023-04-13 21:47:37 +0200

    Update inmers.py

--- inmers.py
+++ inmers.py
@@ -50,7 +50,8 @@ def add_message(message, initialtime):
     now = datetime.now()
     time = now.strftime("%H-%M-%S")
     date = now.strftime("%Y-%m-%d")
-    repo_dir = os.path.join(os.path.abspath(__file__)[:-10].split("\n")[0], "conversations")
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    repo_dir = os.path.join(script_dir, "conversations")
 
     if platform.system() == "Windows":
         x = repo_dir + "\\" + date
@@ -76,16 +77,14 @@ def inmers_chat(message, initial_time):
 
         input_box.send_keys(message + Keys.RETURN)
 
-        time.sleep(20)
-
+        time.sleep(15)
         # Wait for the bot to respond
         WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'span.mwai-text p')))
         all_responses = driver.find_elements(By.CSS_SELECTOR, 'div.mwai-reply')
 
         bot_text_responses = []
-
         for xd in all_responses:
-            if xd.text not in bot_text_responses and "Inmers" in xd.text and xd.text != "Inmers\nHola, mi nombre es Inmers y soy una Inteligencia Artificial muy avanzada. ¿En que te puedo ayudar?":
+            if xd.text not in bot_text_responses and "Inmers:" in xd.text and xd.text != "Inmers:\nBienvenido a la inteligencia artificial mas avanzada del mundo, ¿En que te puedo ayudar?":
                 bot_text_responses.append(xd.text)
 
         index = len(bot_text_responses) - 1
@@ -105,15 +104,14 @@ def inmers_chat(message, initial_time):
                 add_message(bot_response, initial_time)
                 print(colorama.Fore.GREEN + "\n" + bot_response)
                 print(colorama.Fore.RESET)
-        driver.quit()
-    except:
+    except Exception as e:
+        print("Exception", repr(e))
         print("CLOSING...")
         driver.quit()
         exit(1)
 
 
-def inmers():
-    initial_time = datetime.now().strftime("%m-%d-%Y_%H-%M-%S")
+def inmers(initial_time):
     while True:
         print(colorama.Fore.RED + "Enter your text:" + colorama.Fore.CYAN)
         inmers_chat(input(), initial_time)
@@ -130,7 +128,8 @@ def main():
     if len(sys.argv) == 2:
         inmers_chat(sys.argv[1], initial_time)
     else:
-        inmers()
+        inmers(initial_time)
+        driver.quit()
 
 
 if __name__ == '__main__':
